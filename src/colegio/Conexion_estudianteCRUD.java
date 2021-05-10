2a43041251449167e004801ab5b2da0b428f1064
NoelSorianoC <82602830+NoelSorianoC@users.noreply.github.com>
2021-05-09 18:48:58 -0600

    Add files via upload

--- src/colegio/Conexion_estudianteCRUD.java
+++ src/colegio/Conexion_estudianteCRUD.java
@@ -1,14 +1,15 @@
 package colegio;
-import java.sql.Connection;
+import java.sql.*;
 
 
-public class Conexion_estudianteCRUD {
+public class Conexion_estudianteCRUD
+{
     
  private final String servidor = "jbdc:mysql://127.0.0.1:3306/bd_colegio";
  private final int id = 00;
  private final String carnet_estudiante = "";
- private final String nom_estudiante = "Carlos";
- private final String ape_estudiante = "Campos";
+ private final String nom_estudiante = "";
+ private final String ape_estudiante = "";
  private final int carnet = 001;
  private final String DriverConector = "com.mysql.jbdc.Driver";
  private static Connection conexion;
@@ -16,41 +17,44 @@ public class Conexion_estudianteCRUD {
     public Conexion_estudianteCRUD(){
        
         try{
-            class.forName(DriverConector)
-                    
-            conexion= DriverMannager.getConecction(Servidor, nom_estudiante);
-        }catch (ClassNoFoundException | SQLException e){
-            System.out.println("Conexion fallida! Error!: "+ e.getMessage());
+            Class.forName(DriverConector);     
+            conexion=DriverManager.getConnection(servidor, carnet_estudiante, nom_estudiante);
+        }catch (ClassNotFoundException | SQLException e){
+            System.out.println("");
         }
         
-    }   
+    } 
+
     
     public Connection getConnection (){
         return conexion;
     }
     
-    public Void GuardadRegistro(Stringtabla, StringCamposTabla, StringValorestabla){
-        Conexion_estudianteCRUD conectar = New Conexion_estudianteCRUD;
-        conection cone = conectar.getConnection;
-    }
+    public Void GuardadRegistro(String tabla, String CamposTabla, String Valorestabla){
+      
+        Conexion_estudianteCRUD conectar = new Conexion_estudianteCRUD();
+        Connection cone = conectar.getConnection();
+    
     
     try{
     
         String SqlQueryStmt = "INSERT" + tabla + "("+ CamposTabla+ ") VALUES ("+Valorestabla+");";
         
         Statement stmt;
-        
-         stmt = cone.CreateStatement();
-         stmt = execute.Update(SqlQueryStmt);
+         stmt = cone.createStatement();
+         stmt.executeUpdate(SqlQueryStmt);
          stmt.close();
          cone.close();
          
-         {     System.out.println("Registro guardado correctamente ");
+             System.out.println("Registro guardado correctamente ");
           
-}  
+  
     }catch (Exception e){
     
-        System.out.println("e.getMessage()");
+        System.out.println(e.getMessage());
+    }
+
     
+
     
- }
+
